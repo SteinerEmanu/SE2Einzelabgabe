aaa6f6092869888c0cfe6d64e01056e20d8c23bd
SteinerEmanu <56165957+SteinerEmanu@users.noreply.github.com>
2020-03-05 12:33:33 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a00960952_steineremanuel_se2_einzelabgabe/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a00960952_steineremanuel_se2_einzelabgabe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
