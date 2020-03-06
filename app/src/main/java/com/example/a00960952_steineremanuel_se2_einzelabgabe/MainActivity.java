1ea86fa3c50445c4e4f8d614a7f5c7e8da137667
SteinerEmanu <56165957+SteinerEmanu@users.noreply.github.com>
2020-03-06 20:05:06 +0100

    Implemented Button Listener

--- app/src/main/java/com/example/a00960952_steineremanuel_se2_einzelabgabe/MainActivity.java
+++ app/src/main/java/com/example/a00960952_steineremanuel_se2_einzelabgabe/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.a00960952_steineremanuel_se2_einzelabgabe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_sendNumber;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_sendNumber = (Button) findViewById(R.id.btn_sendNumber);
+        btn_sendNumber.setOnClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO
     }
 }
