52c86e73bc2be33cd69b11ee36ea62b2e4a564d4
SteinerEmanu <56165957+SteinerEmanu@users.noreply.github.com>
2020-03-10 19:52:16 +0100

    in progress sorting...

--- app/src/main/java/com/example/a00960952_steineremanuel_se2_einzelabgabe/SortMatrNumber.java
+++ app/src/main/java/com/example/a00960952_steineremanuel_se2_einzelabgabe/SortMatrNumber.java
@@ -8,12 +8,27 @@ public class SortMatrNumber {
         this.number = number;
     }
 
-    public String getSortedNumber (){ // 00960952 mod 7 = 6
+    public String getSortedNumber() { // 00960952 mod 7 = 6
         // Sortiere gerade Ziffern links danach ungerade Ziffern rechts
         String sortedNumber = "";
+        char[] array = number.toCharArray();
+        char swap = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] < array[i + 1]) {
+                swap = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = swap;
+            }
+        }
 
-        
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] % 2 == 1) {
+                
+            }
+        }
+
 
         return sortedNumber;
     }
+
 }
