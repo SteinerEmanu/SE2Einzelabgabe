12a191d2149025e4a854868806aff3d3cb51fa9b
SteinerEmanu <56165957+SteinerEmanu@users.noreply.github.com>
2020-03-10 19:42:42 +0100

    Starte Aufgabe 2 = Restklasse [6]

---
+++ app/src/main/java/com/example/a00960952_steineremanuel_se2_einzelabgabe/SortMatrNumber.java
@@ -0,0 +1,19 @@
+package com.example.a00960952_steineremanuel_se2_einzelabgabe;
+
+public class SortMatrNumber {
+
+    String number;
+
+    public SortMatrNumber(String number) {
+        this.number = number;
+    }
+
+    public String getSortedNumber (){ // 00960952 mod 7 = 6
+        // Sortiere gerade Ziffern links danach ungerade Ziffern rechts
+        String sortedNumber = "";
+
+        
+
+        return sortedNumber;
+    }
+}
